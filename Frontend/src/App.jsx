c2a0508a60c175ed5145f8d6588b309c41e43b93
tanishq singhal <ts2311180@gmail.com>
2026-07-30 20:57:44 +0530

    Fixed frontend deployment issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
-import "./styles/Theme.css";
+import "./Styles/Theme.css";
 import Navbar from "./Components/Navbar";
 import ProtectedRoute from "./Components/ProtectedRoutes";
 import Home from "./Pages/Home";
